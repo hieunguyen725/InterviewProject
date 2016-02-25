0d20bc2e3f0158cc3214c5ebca52893bdb205db5
Long Nguyen <conchutgidenho2290@gmail.com>
2016-02-25 01:38:38 -0800

    fixed some issues

--- Interview/App_Start/RouteConfig.cs
+++ Interview/App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace Interview
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "Users",
+                url: "Users/{action}/{username}",
+                defaults: new { controller = "Users", action = "AllPosts", username = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
